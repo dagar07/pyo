7c9e695bfd2ca29f3aacce6f44549a991672d7f3
tilak <tpymca07@gmail.com>
2020-02-09 20:58:09 +0530

    Add reverse function for py

---
+++ reverse_string.py
@@ -0,0 +1,17 @@
+# this function reverse a string like
+# I Love You => You Love I
+def reverse_string (str):
+  if not bool(str):
+    return 'Invalid String'
+  
+  word = ''
+  reversStr = ''
+  for letter in str:
+    word = word + letter
+    if letter == ' ':
+      reversStr = word + reversStr
+      word= ''
+  reversStr = word + ' ' + reversStr
+  return reversStr
+
+print(reverse_string('Main tum ho'))
